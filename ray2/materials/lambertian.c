286fa986717415b6c74d18545881b35ed7b80f8d
Soul Lee <alus20x@gmail.com>
2023-04-29 19:52:09 +0900

    add near zero in lambertian

--- ray2/materials/lambertian.c
+++ ray2/materials/lambertian.c
@@ -6,7 +6,7 @@
 /*   By: soulee <soulee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 19:47:06 by soulee            #+#    #+#             */
-/*   Updated: 2023/04/29 19:50:56 by soulee           ###   ########.fr       */
+/*   Updated: 2023/04/29 19:51:49 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	lambertian_scatter(t_ray *r_in, t_hit_record *rec,
 	t_vec3	scatter_direction;
 
 	scatter_direction = add_vec3(rec->normal, random_in_unit_sphere());
+	if (near_zero(scatter_direction))
+		scatter_direction = rec->normal;
 	ray.origin = rec->p;
 	ray.direction = scatter_direction;
 	*scattered = ray;
