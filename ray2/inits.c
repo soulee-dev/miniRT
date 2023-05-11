1b52aea2a305ea3a064efda4475eeae205ae4e58
soulee <me@soulee.dev>
2023-05-11 17:32:26 +0900

    change material

--- ray2/inits.c
+++ ray2/inits.c
@@ -6,7 +6,7 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 10:54:38 by soulee            #+#    #+#             */
-/*   Updated: 2023/05/07 15:15:52 by soulee           ###   ########.fr       */
+/*   Updated: 2023/05/11 17:31:48 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ void	init_world(t_env *env)
 	env->world.objects[env->world.size].type = SHAPE_SPHERE;
 	env->world.objects[env->world.size].sphere.center = create_vec3_xyz(0, -1000, 0);
 	env->world.objects[env->world.size].sphere.radius = 1000;
-	env->world.objects[env->world.size].sphere.mat_ptr = material_sphere;
+	env->world.objects[env->world.size].sphere.mat_ptr = material_ground;
 
 	env->world.size++;
 	env->world.objects[env->world.size].type = SHAPE_CYLINDER;
