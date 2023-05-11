aa45d2636beb86a019fe5921af4275f79969f5a2
soulee <me@soulee.dev>
2023-05-11 17:32:50 +0900

    fix norminette

--- ray2/materials/lambertian.c
+++ ray2/materials/lambertian.c
@@ -6,7 +6,7 @@
 /*   By: soulee <soulee@studnet.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 19:47:06 by soulee            #+#    #+#             */
-/*   Updated: 2023/05/04 17:30:13 by soulee           ###   ########.fr       */
+/*   Updated: 2023/05/11 17:32:43 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	lambertian_scatter(t_ray *r_in, t_hit_record *rec,
 	t_vec3	scatter_direction;
 
 	(void) r_in;
-
 	scatter_direction = add_vec3(rec->normal, random_in_unit_sphere());
 	if (near_zero(scatter_direction))
 		scatter_direction = rec->normal;
