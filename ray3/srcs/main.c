f0609324c9a1c00ab271618ab1c545db09396061
Soul Lee <alus20x@gmail.com>
2023-05-25 21:39:44 +0900

    add comments

--- ray3/srcs/main.c
+++ ray3/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: soulee <soulee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 18:24:44 by soulee            #+#    #+#             */
-/*   Updated: 2023/05/25 21:15:12 by soulee           ###   ########.fr       */
+/*   Updated: 2023/05/25 21:38:46 by soulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,20 @@ t_scene	*scene_init(void)
 	if (!scene)
 		exit_error("error: malloc failed (scene)");
 	scene->canvas = canvas(400, 300);
-	scene->camera = camera(&scene->canvas, point3(26, 10, 6), point3(0, 5, 0), 30);
 	scene->mlx = mlx_init();
 	scene->mlx_win = mlx_new_window(scene->mlx, scene->canvas.width, \
 		scene->canvas.height, TITLE);
-	world = object(SP, sphere(point3(0, 0, 0), 2), color(0.5, 0, 0));
-	add_obj(&world, object(SP, sphere(point3(0, -1000, 0), 995), color(1, 1, 1)));
-	add_obj(&world, object(CY, cylinder(point3(2, -1, -5), vec3(0, 1, 0), 1, 5), color(0, 0.5, 0)));
-	add_obj(&world, object(PL, plane(point3(0, 0, 0), vec3(0, 1, 0)), color(0, 0, 1)));
-	scene->world = world;
-	lights = object(LIGHT_POINT, light_point(point3(0, 20, 0), color(1, 1, 1), 0.5), color(0, 0, 0));
-	scene->lights = lights;
-	ka = 0.1;
-	scene->ambient = mul_n_vec3(color(1, 1, 1), ka);
+	// scene->camera = camera(&scene->canvas, point3(26, 10, 6), point3(0, 5, 0), 30);
+	// world = object(SP, sphere(point3(0, 0, 0), 2), color(0.5, 0, 0));
+	// add_obj(&world, object(SP, sphere(point3(0, -1000, 0), 995), color(1, 1, 1)));
+	// add_obj(&world, object(CY, cylinder(point3(2, -1, -5), vec3(0, 1, 0), 1, 5), color(0, 0.5, 0)));
+	// add_obj(&world, object(PL, plane(point3(0, 0, 0), vec3(0, 1, 0)), color(0, 0, 1)));
+	// scene->world = world;
+	// // change parameter order of light_point.
+	// lights = object(LIGHT_POINT, light_point(point3(0, 20, 0), color(1, 1, 1), 0.5), color(0, 0, 0));
+	// scene->lights = lights;
+	// ka = 0.1;
+	// scene->ambient = mul_n_vec3(color(1, 1, 1), ka);
 	return (scene);
 }
 
