d893956e284c53ea6cb568a678388d1f848756e9
Soul Lee <alus20x@gmail.com>
2023-05-25 22:02:19 +0900

    add parser functions

---
+++ ray3/srcs/parser/parser1.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soulee <soulee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 21:57:36 by soulee            #+#    #+#             */
+/*   Updated: 2023/05/25 22:00:48 by soulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "structures.h"
+#include "utils.h"
+
+void	add_ambient_light(t_scene *scene, char **strs)
+{
+}
+
+void	add_camera(t_scene *scene, char **strs)
+{
+}
+
+void	add_light(t_scene *scene, char **strs)
+{
+}
---
+++ ray3/srcs/parser/parser2.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soulee <soulee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 21:57:34 by soulee            #+#    #+#             */
+/*   Updated: 2023/05/25 22:01:33 by soulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "structures.h"
+#include "utils.h"
+
+void	add_sphere(t_scene *scene, char **strs)
+{
+}
+
+void	add_plane(t_scene *scene, char **strs)
+{
+	
+}
+
+void	add_cylinder(t_scene *scene, char **strs)
+{
+	
+}
